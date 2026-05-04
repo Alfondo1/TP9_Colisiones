6dbb0ae7a2d0b98c195f62442d90b049ee33af5c
Alfondo1 <50048555@est.ort.edu.ar>
2026-05-04 10:38:05 -0300

    Initial commit

---
+++ Assets/Scrypts/InteractionArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public ScoreUIManager scoreUIMGR;
+    void OnCollisionEnter(Collision col)
+    {
+
+        if(col.gameObject.CompareTag("Pickable"))
+        {
+            Debug.Log("Hubo colisión con un Pickable");
+            Destroy(col.gameObject);
+            scoreUIMGR.AddPointAndUpdateScoreUI();
+        }
+    }
+}
